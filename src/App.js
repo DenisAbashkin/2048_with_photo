eccb108dba7f12f27262d347a7688fbd1eeac892
abashkin <denisabahkin5@gmail.com>
2022-06-26 01:17:50 +0300

    (Build) Текстовый вариант без плиточного представления

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ]);
+
+    return (
+        <div
+            style={{
+                background: "#AD9D8F",
+                width: "max-content",
+                margin: "auto",
+                padding: 5,
+                borderRadius: 5,
+                marginTop: 10,
+            }}>
+
+            {data.map((row, oneIndex) => {
+                return (
+                    <div style={{display: "flex"}} key={oneIndex}>
+                        {row.map((digit, index) => (
+                            <Block num={digit} key={index}/>
+                        ))}
+                    </div>
+                );
+            })
+            }
+        </div>
+    );
 }
+
+const Block = ({num}) => {
+    return <div>{num}</div>
+};
+
 
 export default App;
